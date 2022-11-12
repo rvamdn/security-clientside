339248f8a6399d5291254b2ef4efc2716cbcc505
irvan ramadan <ir.irvanramadan@gmail.com>
2022-11-12 17:52:08 +0700

    first commit

---
+++ src/main/java/co/id/mii/security/clientside/ClientsideApplication.java
@@ -0,0 +1,13 @@
+package co.id.mii.security.clientside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/mii/security/clientside/ClientsideApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.mii.security.clientside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
